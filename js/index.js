c7aaa65e412c9d3d5904a12bccd5d0b7a871bab8
Keyla Juleyma Cardenas Esteban <keyla.16f@gmail.com>
2022-11-28 11:01:52 -0500

    fonts

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import scrollTopButton from '../js/scroll.js';
+import slider from './slider.js';
 
 const d = document;
 
 d.addEventListener('DOMContentLoaded', (e) => {
     scrollTopButton('.scroll-top-btn');
+    slider();
 });
